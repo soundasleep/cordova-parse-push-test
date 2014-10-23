8a5b6392f8f3bc75324b462e96a5709f12e5cbc8
Jevon Wright <jevon@jevon.org>
2014-10-23 15:33:50 +1300

    Adding test buttons

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -4,9 +4,26 @@ document.addEventListener("deviceready", function() {
 	$(document).ready(function() {
 		parsePushPlugin.initialize("IJ1M9zwnbfYIhsd4WIe9R9jSLr6Amt6KVrJZSaz3", "szZJuqN4y0Sa40NTPJROZTXikUtS1K33bviXNZcA", 
 			function(success) { 
-				alert("success: " + success);
+				alert("Initialize success: " + success);
 			}, function(failure) {
-				alert("failure: " + failure);
+				alert("Initialize failure: " + failure);
 			});
+		
+		success = function(m) { alert("Success: " + m); };
+		failure = function(m) { alert("Failure: " + m); };
+	
+		$("button.subscribe").click(function() {
+			parsePushPlugin.subscribe("testChannel", success, failure);
+		});
+		$("button.unsubscribe").click(function() {
+			parsePushPlugin.unsubscribe("testChannel", success, failure);
+		});
+		$("button.getSubscriptions").click(function() {
+			parsePushPlugin.getSubscriptions(function (channels) {
+				alert("Channels subscribed to: " + channels);
+			});
+		});
+		
+		$(".wait-for-ready").show();
 	});	
 }, false);
--- www/js/index.js
+++ www/js/index.js
@@ -8,5 +8,22 @@ document.addEventListener("deviceready", function() {
 			}, function(failure) {
 				alert("Initialize failure: " + failure);
 			});
+		
+		success = function(m) { alert("Success: " + m); };
+		failure = function(m) { alert("Failure: " + m); };
+	
+		$("button.subscribe").click(function() {
+			parsePushPlugin.subscribe("testChannel", success, failure);
+		});
+		$("button.unsubscribe").click(function() {
+			parsePushPlugin.unsubscribe("testChannel", success, failure);
+		});
+		$("button.getSubscriptions").click(function() {
+			parsePushPlugin.getSubscriptions(function (channels) {
+				alert("Channels subscribed to: " + channels);
+			});
+		});
+		
+		$(".wait-for-ready").show();
 	});	
 }, false);
