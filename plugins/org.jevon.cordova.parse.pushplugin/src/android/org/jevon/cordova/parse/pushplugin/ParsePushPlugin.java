eede4bc9de7575c76ce0a814b567f1316a359ad8
Jevon Wright <jevon@jevon.org>
2014-10-23 15:16:49 +1300

    Updating to latest cordova-parse-push plugin

--- plugins/org.jevon.cordova.parse.pushplugin/src/android/org/jevon/cordova/parse/pushplugin/ParsePushPlugin.java
+++ plugins/org.jevon.cordova.parse.pushplugin/src/android/org/jevon/cordova/parse/pushplugin/ParsePushPlugin.java
@@ -5,6 +5,8 @@ import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import com.parse.Parse;
+
 public class ParsePushPlugin extends CordovaPlugin {
 	
 	public static final String ACTION_INITIALIZE = "initialize";
@@ -16,14 +18,17 @@ public class ParsePushPlugin extends CordovaPlugin {
 			return true;
 		}
 		// TODO more actions...
-		return false;
+		return false;	// false == NoSuchMethod
 	}
 	
-	private void initialize(final CallbackContext context, final JSONArray args) {
+	private void initialize(final CallbackContext context, final JSONArray args) throws JSONException {
 		if (args.length() >= 1) {
-			context.success("success");
+			String appId = args.getString(0);
+			String clientKey = args.getString(1);
+			Parse.initialize(cordova.getActivity(), appId, clientKey);
+			context.success();
 		} else {
-			context.error("no args specified");
+			context.error("Expected two arguments");
 		}
 	}
 
